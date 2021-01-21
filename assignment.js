1d4c1d1d61584ccc9e91d2284bd77a5a46c5b0b2
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-01-22 03:35:51 +0600

    Third funtion added

--- assignment.js
+++ assignment.js
@@ -16,4 +16,28 @@ function budgetCalculator(watch, phone, laptop) {
     return total;
 }
 var result = budgetCalculator(2, 1, 1)
+console.log(result); //delete
+
+//Hotel room cost function
+
+function hotelCost(day) {
+    var cost = "";
+
+    if (day <= 10) {
+        cost = day * 100;
+    } else if (day <= 20) {
+        var firstPart = 10 * 100;
+        var remain = day - 10;
+        var secondPart = remain * 80;
+        cost = firstPart + secondPart;
+    } else {
+        var firstPart = 10 * 100;
+        var secondPart = 20 * 80;
+        var remain = day - 20;
+        var thirdPart = remain * 50;
+        cost = firstPart + secondPart + thirdPart;
+    }
+    return cost;
+}
+var result = hotelCost(30);
 console.log(result);
