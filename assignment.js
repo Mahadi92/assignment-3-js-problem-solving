c98d720c137315afe0955523a6ba46ca11d7a214
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-01-22 16:14:00 +0600

    Fourth function added valitation added

--- assignment.js
+++ assignment.js
@@ -1,9 +1,15 @@
 // Kilometer To Meter
 
 function kilometerToMeter(kilo) {
-    return kilo * 1000;
+    if (valitation(kilo)) {
+        var meter = kilo * 1000;
+    } else {
+        var meter = "Enter youer valid number !!! ";
+    }
+    return meter;
 }
-console.log(kilometerToMeter(12)); //delete
+console.log(kilometerToMeter(3))
+
 
 //Budget Calculator
 
@@ -12,32 +18,66 @@ function budgetCalculator(watch, phone, laptop) {
     var phonePrice = 100;
     var laptopPrice = 500;
 
-    var total = (watch * watchPrice) + (phone * phonePrice) + (laptop * laptopPrice);
+    if (valitation(watch) && valitation(phone) && valitation(laptop)) {
+        var total = (watch * watchPrice) + (phone * phonePrice) + (laptop * laptopPrice);
+    } else {
+        total = "Enter your valid Price !!! ";
+    }
     return total;
 }
-var result = budgetCalculator(2, 1, 1)
-console.log(result); //delete
+console.log(budgetCalculator(5, 'd', 2));
 
 //Hotel room cost function
 
 function hotelCost(day) {
     var cost = "";
 
-    if (day <= 10) {
-        cost = day * 100;
-    } else if (day <= 20) {
-        var firstPart = 10 * 100;
-        var remain = day - 10;
-        var secondPart = remain * 80;
-        cost = firstPart + secondPart;
+    if (valitation(day)) {
+
+        if (day <= 10) {
+            cost = day * 100;
+        } else if (day <= 20) {
+            var firstPart = 10 * 100;
+            var remain = day - 10;
+            var secondPart = remain * 80;
+            cost = firstPart + secondPart;
+        } else {
+            var firstPart = 10 * 100;
+            var secondPart = 20 * 80;
+            var remain = day - 20;
+            var thirdPart = remain * 50;
+            cost = firstPart + secondPart + thirdPart;
+        }
     } else {
-        var firstPart = 10 * 100;
-        var secondPart = 20 * 80;
-        var remain = day - 20;
-        var thirdPart = remain * 50;
-        cost = firstPart + secondPart + thirdPart;
+        cost = "Enter your valid cost";
     }
     return cost;
 }
-var result = hotelCost(30);
-console.log(result);
+
+
+
+// Mega Friend function
+
+function megaFriend(friends) {
+    var maxName = friends[0];
+    for (var i = 0; i < friends.length; i++) {
+        var nameChar = friends[i];
+
+        if (friends[i].length > maxName.length) {
+            maxName = nameChar;
+        }
+    }
+    return maxName;
+}
+// var result = megaFriend(['madi', 'aminulIslam', 'nam', 'mahin', 'zahidCowd'])
+// console.log(result);
+
+//Valitation  function
+
+function valitation(condition) {
+    if (condition > 0 && typeof (condition) == 'number') {
+        return true;
+    } else {
+        return false;
+    }
+}
